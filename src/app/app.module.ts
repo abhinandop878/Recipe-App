741aa400aa000134768582d784614446a887d5dc
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-20 00:13:45 +0530

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { ViewrecipeComponent } from './viewrecipe/viewrecipe.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:"",component:AddRecipeComponent
+  },
+  {
+    path:"viewRecipe",component:ViewrecipeComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +24,8 @@ import { ViewrecipeComponent } from './viewrecipe/viewrecipe.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
