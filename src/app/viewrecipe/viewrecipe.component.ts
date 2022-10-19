37d4cc19eede6dc887e5b8bd3de7c185661353a9
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-20 00:23:31 +0530

    api

--- src/app/viewrecipe/viewrecipe.component.ts
+++ src/app/viewrecipe/viewrecipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-viewrecipe',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewrecipeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { 
+    this.fetchData()
+  }
   status:boolean=false
-  recipeData=[]
+  fetchData=()=>{
+    this.myapi.viewRecipe().subscribe((data)=>{
+      this.recipeData=data
+      this.status=true
+    })
+  }
+  recipeData:any=[]
   ngOnInit(): void {
   }
 
